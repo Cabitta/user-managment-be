8e85ca012b168cc3bc52ada1343190f0b8432526
Franco Cabanillas <fcabanillas@uade.edu.ar>
2026-02-28 00:06:55 -0300

    fix: mongoose pre-save hook for password hashing

--- src/models/user.model.js
+++ src/models/user.model.js
@@ -50,14 +50,13 @@ const userSchema = new mongoose.Schema(
 );
 
 // Hook: Hashear contraseña antes de guardar si fue modificada
-userSchema.pre('save', async function (next) {
+userSchema.pre('save', async function () {
   if (!this.isModified('password')) {
-    return next();
+    return;
   }
 
   const salt = await bcrypt.genSalt(10);
   this.password = await bcrypt.hash(this.password, salt);
-  next();
 });
 
 // Método para comparar contraseñas
